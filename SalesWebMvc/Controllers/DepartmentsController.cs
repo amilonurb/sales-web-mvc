0f244f8c24f63d01b1f33653d33543fcd86c5004
Bruno Rodrigues Lima <brunolima.18@outlook.com>
2021-01-06 17:08:33 -0300

    feat: add DepartmentsController

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var departments = new List<Department>
+            {
+                new Department { Id = 1, Name = "Electronics" }
+            };
+            return View(departments);
+        }
+    }
+}
