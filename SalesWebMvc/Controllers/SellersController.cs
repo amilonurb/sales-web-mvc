7fe840f28cd1b70faf00f0e7feac3df68083681d
Bruno Rodrigues Lima <brunolima.18@outlook.com>
2021-01-07 13:17:51 -0300

    feat: add SellersService

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SellersService _sellersService;
+
+        public SellersController(SellersService sellersService)
+        {
+            _sellersService = sellersService;
+        }
+
         public IActionResult Index()
         {
+            var sellers = _sellersService.FindAll();
+            return View(sellers);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellersService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ SalesWebMvc/Services/SellersService.cs
@@ -0,0 +1,25 @@
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Services
+{
+    public class SellersService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SellersService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll() => _context.Seller.ToList();
+
+        public void Insert(Seller seller)
+        {
+            _context.Add(seller);
+            _context.SaveChanges();
+        }
+    }
+}
